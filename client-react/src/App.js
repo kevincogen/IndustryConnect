b8f2b62cf454e7fde09ffef4ba020822f03cc393
Kevin Cogen <k.cogen@gmail.com>
2023-07-26 20:28:59 -0400

    adding auth0 to client-react

--- client-react/src/App.js
+++ client-react/src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import LoginButton from './components/buttons/login-button';
+import LogoutButton from './components/buttons/logout-button';
+import Profile from './components/pages/profile';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         IndustryConnect Skeleton
+        <LoginButton />
+        <LogoutButton />
+        <Profile />
       </header>
     </div>
   );
