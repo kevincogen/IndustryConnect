98e71b31ef3fc7ddcf3b4716a46186cf17c5869a
Kevin Cogen <k.cogen@gmail.com>
2023-07-28 16:05:42 -0400

    API profiles routes

--- server-node/server.js
+++ server-node/server.js
@@ -9,33 +9,21 @@ const PORT = process.env.PORT || 8080;
 const app = express();
 
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
-// 'dev' = Concise output colored by response status for development use.
-//         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 // Separated Routes for each Resource
-// Note: Feel free to replace the example routes below with your own
 const userApiRoutes = require('./routes/users-api');
 const widgetApiRoutes = require('./routes/widgets-api');
 const usersRoutes = require('./routes/users');
+const profilesRoutes = require('./routes/profiles'); // Require the profiles routes
 
 // Mount all resource routes
-// Note: Feel free to replace the example routes below with your own
-// Note: Endpoints that return data (eg. JSON) usually start with `/api`
 app.use('/api/users', userApiRoutes);
 app.use('/api/widgets', widgetApiRoutes);
 app.use('/users', usersRoutes);
-// Note: mount other resources here, using the same pattern above
-
-// Home page
-// Warning: avoid creating more routes in this file!
-// Separate them into separate routes files (see above).
-
-// app.get('/', (req, res) => {
-//   res.render('index');
-// });
+app.use('/api/profiles', profilesRoutes); // Mount the profiles routes
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
