ff2b6b94ee591d68a697c3a1aa1856198fdef420
Kevin Cogen <k.cogen@gmail.com>
2023-08-04 11:48:42 -0400

    updated pass route for API

--- server-node/routes/pass.js
+++ server-node/routes/pass.js
@@ -2,8 +2,28 @@ const express = require('express');
 const router = express.Router();
 const db = require('../db/connection');
 
-// router.get('/', async (req, res) => {
+router.put('/', async (req, res) => {
 
-// });
+  const authenticationId = req.headers['x-auth0-sub'];
+  const userIdToPass = req.body.UserID;
+
+  if (!authenticationId || !userIdToPass) {
+    return res.status(400).json({ error: 'Authentication ID or User ID to pass is missing' });
+  }
+
+  const query = `
+    UPDATE users
+    SET passes = array_append(passes, $1)
+    WHERE authentication_id = $2
+    RETURNING passes;
+  `;
+
+  try {
+    const result = await db.query(query, [userIdToPass, authenticationId]);
+    return res.json(result.rows[0].passes);
+  } catch (err) {
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
+});
 
 module.exports = router;
