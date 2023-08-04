c30c8a12e6a768d1085691996fd5cf07f9d0c985
Mike <mike.simnes@gmail.com>
2023-08-04 14:17:52 -0700

    Add browser router.

--- client-react/src/index.js
+++ client-react/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './index.css';
 import App from './App';
 import { Auth0Provider } from '@auth0/auth0-react';
