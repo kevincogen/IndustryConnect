2ad7cf57ba2b3455a8e3fe7958cf657cd4074fb9
Mike <mike.simnes@gmail.com>
2023-08-05 14:55:09 -0700

    establish connection/disconnection

--- client-react/src/pages/chat.js
+++ client-react/src/pages/chat.js
@@ -1,22 +1,42 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { io } from "socket.io-client";
+import { 
+  Grid, 
+  Typography,
+  Button,
+  TextField,
+
+} from '@mui/material';
+
 
 export default function Chat() {
-
+  const [messages, setMessages] = useState([]); // [ {user: "user", message: "message"}, {user: "user", message: "message"}
+   
+  
 
   // establish socket connection
   useEffect(() => {
     const socket = io("http://localhost:3000");
+    
     socket.on("connect", () => {
       console.log("connection made")
     })
+
+    socket.on("system", (data) => {
+      console.log(data)
+    })
+
+    // disconnect socket connection
+    return () => {
+      socket.disconnect();
+    }
+
   }, []);
 
     return (
-        <div>
-            <h1>Chat</h1>
 
-        </div>
+      <h1>CHAT</h1>
+          
     )
 }
 
