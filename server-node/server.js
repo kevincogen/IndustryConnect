e043744a5d69760fdabd53142d9ee0fd3ed49a94
Mike <mike.simnes@gmail.com>
2023-08-04 18:00:53 -0700

    Remove repeat const cors declaration.

--- server-node/server.js
+++ server-node/server.js
@@ -5,7 +5,6 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
-const cors = require('cors');
 
 const PORT = process.env.PORT || 8080;
 const app = express();
