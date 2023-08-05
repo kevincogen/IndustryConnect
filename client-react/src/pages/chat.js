f4409f7d07cf95ca0e19fdffc5f02fcffe12ec13
Mike <mike.simnes@gmail.com>
2023-08-05 09:57:15 -0700

    create export default function Chat

---
+++ client-react/src/pages/chat.js
@@ -0,0 +1,16 @@
+import React, { useEffect } from 'react'
+// import { io } from "socket.io-client";
+
+export default function Chat() {
+  // useEffect(() => {
+  //   const socket = io();
+  // }, []);
+
+
+    return (
+        <div>
+            <h1>Chat</h1>
+        </div>
+    )
+}
+
