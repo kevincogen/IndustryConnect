70c45b70fb0537f3961a24078df639004b8124ef
Mike <mike.simnes@gmail.com>
2023-08-05 15:21:21 -0700

    modify seed file to include industry.

--- server-node/db/seeds/01_users.js
+++ server-node/db/seeds/01_users.js
@@ -10,6 +10,40 @@ const pool = new Pool({
   port: 5432,
 });
 
+// -- List of industry categories
+const industryCategories = [
+  "Information Technology",
+  "Healthcare",
+  "Finance",
+  "Education",
+  "Marketing",
+  "Sales",
+  "Engineering",
+  "Human Resources",
+  "Design",
+  "Business Development",
+  "Customer Service",
+  "Project Management",
+  "Consulting",
+  "Media and Communication",
+  "Legal Services",
+  "Accounting",
+  "Administration",
+  "Research and Development",
+  "Manufacturing",
+  "Retail",
+  "Hospitality and Tourism",
+  "Real Estate",
+  "Nonprofit and Social Services",
+  "Government and Public Administration",
+  "Art and Entertainment",
+  "Science and Technology",
+  "Environmental and Sustainability",
+  "Transportation and Logistics",
+  "Architecture and Construction",
+  "Sports and Fitness"
+];
+
 // -- Function to generate random user data
 const generateRandomUser = () => {
   return {
@@ -18,6 +52,7 @@ const generateRandomUser = () => {
     email: faker.internet.email(),
     authentication_id: faker.string.uuid(),
     profile_picture: faker.image.avatar(),
+    industry: industryCategories[Math.floor(Math.random() * industryCategories.length)],
     bio: faker.lorem.paragraph(),
     education: faker.lorem.sentence(),
     experience: faker.person.jobTitle(),
@@ -46,6 +81,7 @@ const seedUsersTable = async () => {
               email,
               authentication_id,
               profile_picture,
+              industry,
               bio,
               education,
               experience,
@@ -56,7 +92,7 @@ const seedUsersTable = async () => {
               website
             )
           VALUES
-            ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)
+            ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
         `,
         values: Object.values(userData),
       };
