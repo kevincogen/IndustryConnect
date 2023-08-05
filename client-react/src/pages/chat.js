14ab06b1e3082e5ceda1bfa26fd898cc65fd0fc7
Mike <mike.simnes@gmail.com>
2023-08-05 10:57:02 -0700

    add socket.io to Chat component.

--- client-react/src/pages/chat.js
+++ client-react/src/pages/chat.js
@@ -1,15 +1,21 @@
 import React, { useEffect } from 'react'
-// import { io } from "socket.io-client";
+import { io } from "socket.io-client";
 
 export default function Chat() {
-  // useEffect(() => {
-  //   const socket = io();
-  // }, []);
 
+
+  // establish socket connection
+  useEffect(() => {
+    const socket = io("http://localhost:3000");
+    socket.on("connect", () => {
+      console.log("connection made")
+    })
+  }, []);
 
     return (
         <div>
             <h1>Chat</h1>
+
         </div>
     )
 }
