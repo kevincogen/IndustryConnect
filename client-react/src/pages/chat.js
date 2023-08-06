7d0025a30d3aca10ad5e62bc46e4fee481b211ea
Mike <mike.simnes@gmail.com>
2023-08-06 13:23:54 -0700

    add chat msg retrieval and listen for new msgs

--- client-react/src/pages/chat.js
+++ client-react/src/pages/chat.js
@@ -1,42 +1,97 @@
 import React, { useEffect, useState } from 'react'
+import { useLocation } from 'react-router-dom';
 import { io } from "socket.io-client";
-import { 
-  Grid, 
-  Typography,
-  Button,
-  TextField,
-
-} from '@mui/material';
-
+import { Grid, Typography, Button, TextField} from '@mui/material';
 
 export default function Chat() {
-  const [messages, setMessages] = useState([]); // [ {user: "user", message: "message"}, {user: "user", message: "message"}
-   
-  
+  const [chatHistory, setChatHistory] = useState([]); 
+  const location = useLocation();
+  const match = location?.state?.match;
+  const currentUserId = location?.state?.currentUserId;  
 
-  // establish socket connection
-  useEffect(() => {
+
+
+useEffect(() => {
+  if (match) {
     const socket = io("http://localhost:3000");
     
-    socket.on("connect", () => {
-      console.log("connection made")
-    })
-
-    socket.on("system", (data) => {
-      console.log(data)
-    })
+    // get chat history from server
+    socket.emit("get_chat_history", match.match_id, (response) => {
+      setChatHistory(response)
+      })
+    
+    // listen for incoming messages
+    socket.on("chat message", (msg) => {
+      setChatHistory((chatHistory) => [...chatHistory, msg]);
+    });
 
     // disconnect socket connection
     return () => {
       socket.disconnect();
     }
+  };
+}, [match]);
 
-  }, []);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    //organize message data
+    const message = event.target.elements.message.value;
+    const sender_id = currentUserId;
+    const receiver_id = match.id;
+    const matchId = match.match_id;
+    const msg = { matchId, sender_id, receiver_id, message };
+    
+    const socket = io("http://localhost:3000");
+    socket.emit("chat message", msg);
+    event.target.reset();
+  };
+
 
     return (
+      <>
+       <Grid container spacing={2}>
+         <Grid item xs={12}>
+           <Typography variant="h4" component="h1" gutterBottom>
+             Chat
+           </Typography>
+         </Grid>
 
-      <h1>CHAT</h1>
-          
+         <Grid item xs={12}>
+           <Typography variant="h5" component="h2" gutterBottom>
+             {match.first_name} {match.last_name}
+           </Typography>
+         </Grid>
+
+         <Grid item xs={12}>
+            <Typography variant="h6" component="h3" gutterBottom>
+              Chat History
+            </Typography>
+            {chatHistory.map((message, index) => (
+              <Typography key={index} variant="body1" gutterBottom>
+                {message.sender}: {message.message}
+              </Typography>
+            ))}
+        </Grid>
+        <form onSubmit={handleSubmit}>
+         <Grid item xs={12}>
+           <TextField
+             id="outlined-multiline-static"
+             label="Message"
+             multiline
+             rows={2}
+             variant="outlined"
+              name="message"
+
+           />
+         </Grid>
+         <Grid item xs={12}>
+           <Button type="submit" variant="contained" color="primary">
+             Send
+           </Button>
+         </Grid>
+         </form>
+       </Grid>
+      </>
     )
 }
 
