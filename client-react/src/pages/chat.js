f46ab0fe42a2b84a1fcff1d94d551150d08eb50b
Mike <mike.simnes@gmail.com>
2023-08-09 19:38:11 -0700

    Include sidebar and load conversation from sidebar

--- client-react/src/pages/chat.js
+++ client-react/src/pages/chat.js
@@ -2,41 +2,55 @@ import React, { useEffect, useState } from 'react'
 import { useLocation } from 'react-router-dom';
 import { io } from "socket.io-client";
 import { Grid, Typography } from '@mui/material';
-import moment from 'moment';
 import { ChatHistoryContainer, ChatBubble, Timestamp, FormContainer, InputField, SendButton } from '../pages/chat-styles';
+import moment from 'moment';
+import axios from 'axios';
+import useGetCurrentUser from '../hooks/useGetCurrentUser';
+import Sidebar from '../components/partials/ConnectSideBar';
+
 
 const formatTimestamp = (timestamp) => {
   return moment(timestamp).fromNow();
 };
 
-
 export default function Chat() {
   const [chatHistory, setChatHistory] = useState([]); 
+  const [matches, setMatches] = useState([]);
+  const [refreshMatches, setRefreshMatches] = useState(0);
+
+  // get match from location state
   const location = useLocation();
   const match = location?.state?.match;
-  const currentUserId = location?.state?.currentUserId;  
 
+  // get current user from custom hook
+  const currentUser = useGetCurrentUser();
+  const currentUserId = currentUser?.[0]?.[0]?.id;
+  
+  
+  useEffect(() => {
+    if (currentUser === null) {
+      return <div>Loading...</div>;
+    } 
+  
+    if (match) {
+      const socket = io("http://localhost:3000");
+      
+      // get chat history from server
+      socket.emit("get_chat_history", match.match_id, (response) => {
+        setChatHistory(response)
+        })
+      
+      // listen for incoming messages
+      socket.on("chat message", (msg) => {
+        setChatHistory((chatHistory) => [...chatHistory, msg]);
+      });
 
-useEffect(() => {
-  if (match) {
-    const socket = io("http://localhost:3000");
-    
-    // get chat history from server
-    socket.emit("get_chat_history", match.match_id, (response) => {
-      setChatHistory(response)
-      })
-    
-    // listen for incoming messages
-    socket.on("chat message", (msg) => {
-      setChatHistory((chatHistory) => [...chatHistory, msg]);
-    });
-
-    // disconnect socket connection
-    return () => {
-      socket.disconnect();
-    }
-  };
-}, [match]);
+      // disconnect socket connection
+      return () => {
+        socket.disconnect();
+      }
+    };
+  }, [match]);
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -52,9 +66,33 @@ useEffect(() => {
     event.target.reset();
   };
 
+  useEffect(() => {
+    const getMatchList = async () => {
+      if (!currentUserId) return;
+      if (currentUserId) {
+        console.log("We have currentUserId: ", currentUserId);
+        try {
+          const response = await axios.get(`http://localhost:8080/api/matchList/${currentUserId}`);
+          setMatches(response.data);
+        } catch (error) {
+          console.error("There was an error retrieving the matches: ", error);
+        }
+      }
+    };
+    getMatchList();
+  }, [currentUserId, refreshMatches]);
 
-    return (
-      <>
+  if (currentUser[0] === null) {
+    console.log("currentUser is null")
+    return <div>Loading...</div>;
+  } 
+  
+  return (
+    <>
+      { currentUser && (
+        <div style={{ display: 'flex', height: '100vh' }}>
+        <Sidebar currentUser={currentUser[0][0]} refreshMatches={refreshMatches}/>
+        <div style={{ flex: 1, padding: '16px', overflowY: 'auto' }}>
        <Grid container spacing={2}>
          <Grid item xs={12}>
            <Typography variant="h4" component="h1" gutterBottom>
@@ -64,7 +102,7 @@ useEffect(() => {
 
          <Grid item xs={12}>
            <Typography variant="h5" component="h2" gutterBottom>
-             {match.first_name} {match.last_name}
+             {match?.first_name || null} {match?.last_name || null}
            </Typography>
          </Grid>
 
@@ -96,7 +134,11 @@ useEffect(() => {
           </SendButton>
         </FormContainer>
        </Grid>
-      </>
-    )
+       </div>
+       </div>
+    ) 
+  }
+  </>
+  );
 }
 
