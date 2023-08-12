d2e8bed07a496589d66c18ada425adb0d6c099ea
Mike <mike.simnes@gmail.com>
2023-08-11 18:55:57 -0700

    create chat animation component for /chat

---
+++ client-react/src/animations/chatAnimation.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Lottie from 'lottie-web';
+import animationData from './chatAnimation.json';
+
+const ChatAnimation = () => {
+  React.useEffect(() => {
+    // Create a Lottie instance
+    const lottieInstance = Lottie.loadAnimation({
+      container: document.getElementById('lottie-container'), 
+      animationData: animationData,
+      loop: true,
+      autoplay: true,
+    });
+
+    // Clean up when the component unmounts
+    return () => {
+      lottieInstance.destroy();
+    };
+  }, []);
+
+  return (
+    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
+      <div id="lottie-container" style={{ width: '80%', maxWidth: '600px', height: 'auto', marginBottom: '32px' }}>
+      </div>
+    </div>
+  );
+};
+
+export default ChatAnimation;
