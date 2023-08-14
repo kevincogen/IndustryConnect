324d441cf9c3c2e25e8e20763b84a68d488eadb3
Mike <mike.simnes@gmail.com>
2023-08-14 15:37:12 -0700

    fix chat width.

--- client-react/src/pages/chat.js
+++ client-react/src/pages/chat.js
@@ -113,7 +113,7 @@ export default function Chat() {
               refreshMatches={refreshMatches}
               style={{ width: '25%' }} 
           />
-          <div style={{ flex: 1, padding: '16px', overflowY: 'auto', display: 'flex', flexDirection: 'column' }}>
+          <div style={{ flex: 1, overflowY: 'auto', display: 'flex', flexDirection: 'column' }}>
             <Grid container spacing={2}>
               <Grid item xs={12}>
                 <Typography variant="h4" component="h1" gutterBottom>
